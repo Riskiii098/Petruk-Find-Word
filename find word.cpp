3141afba4d9664746cb9c2cc642c6b5fbaaf914b
Nafisyaaa <2317051111@students.unila.ac.id>
2024-04-28 17:37:20 +0700

    horizontal dan vertikal

--- find word.cpp
+++ find word.cpp
@@ -8,10 +8,31 @@ bool searchWord(vector<string> matrix, string word);
 	int rows = matrix.size();
 	int cols = matrix[0].size();
 
+    for (int i = 0; i < rows; ++i) {
+        if (matrix[i].find(word) != string::npos) {
+            cout << " Ada" << endl;
+            return true;
+        }
+    }
+
+    for (int j = 0; j < cols; ++j) {
+        string colStr = "";
+        for (int i = 0; i < rows; ++i) {
+            colStr += matrix[i][j];
+        }
+        if (colStr.find(word) != string::npos) {
+            cout << " Ada" << endl;
+            return true;
+        }
+    }
+
+
 int main() {
     int numWords;
     cout << " ";
     cin >> numWords;
+    
+    
 
     vector<string> matrix = {
         "AAFLKHPFSSUFICICLESGNNH",
